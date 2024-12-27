4ec0e46a1f008e30343deff11b784bb58642224b
Sudesh <sudacesrma2058@gmail.com>
2024-12-27 12:06:38 +0545

    BackEnd

--- api/urls.py
+++ api/urls.py
@@ -5,38 +5,40 @@ urlpatterns = [
     # API URL
     
     ########################## Word #######################
-    path("api/word/post/", CreateWord, name="create_post_word"),
-    path("api/word/all/", GetAllWord, name="get_word"),
+    path("api/word/", CreateWord, name="create_post_word"),
+    path("api/all/word/", GetAllWord, name="get_allword"),
     path("api/word/<int:id>/", GetWordById, name="get_wordby_id"),
     path("api/word/update/<int:id>/", UpdateWord, name="update_word"),
     path("api/word/delete/<int:id>/", DeleteWord, name="delete_word"),
     ########################## Word #######################
     
     ####################### POST CATEGORY #######################
-    path("api/postcat/post/", CreatePostCategory, name="create_postcat"),
-    path("api/postcat/all/", GetAllCategories, name="postcat_list"),
+    path("api/postcat/", CreatePostCategory, name="create_postcat"),
+    path("api/all/postcat/", GetAllCategories, name="get_allpostcat"),
     path("api/postcat/<int:id>/", GetCategoriesById, name="get_postcatby_id"),
     path("api/postcat/update/<int:id>/", UpdateCategory, name="update_postcat"),
     path("api/postcat/delete/<int:id>/", DeleteCategory, name="delete_postcat"),
     ####################### POST CATEGORY #######################
     
     ####################### POST #######################
-    path("api/post/post/", CreatePost, name="create_post"),
-    path("api/post/all/", GetAllPost, name="post_list"),
+    path("api/post/", CreatePost, name="create_post"),
+    path("api/all/post/", GetAllPost, name="get_allpost"),
     path("api/post/<int:id>/", GetAllPostById, name="get_postby_id"),
     path("api/post/update/<int:id>/", UpdatePost, name="update_post"),
     path("api/post/delete/<int:id>/", DeletePost, name="delete_post"),
     ####################### POST #######################
+    
     ####################### Footer #######################
-    path("api/footer/post/", CreateFooter, name="create_footer"),
-    path("api/footer/all/", GetAllFooter, name="footer_list"),
+    path("api/footer/", CreateFooter, name="create_footer"),
+    path("api/all/footer/", GetAllFooter, name="get_allfooter"),
     path("api/footer/<int:id>/", GetAllFooterById, name="get_footerby_id"),
     path("api/footer/delete/<int:id>/", DeleteFooter, name="delete_footer"),
     path("api/footer/update/<int:id>/", UpdateFooter, name="update_footer"),
     ####################### Footer #######################
+    
     ####################### Header #######################
-    path("api/header/post/", CreateHeader, name="create_header"),
-    path("api/header/all/", GetAllHeader, name="header_list"),
+    path("api/header/", CreateHeader, name="create_header"),
+    path("api/all/header/", GetAllHeader, name="get_allheader"),
     path("api/header/<int:id>/", GetAllHeaderById, name="get_headerby_id"),
     path("api/header/delete/<int:id>/", DeleteHeader, name="delete_header"),
     path("api/header/update/<int:id>/", UpdateHeader, name="update_header"),
